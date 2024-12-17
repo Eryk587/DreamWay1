3ec1618150d10e5255d26799d9758a887a2522b3
eryk587 <eryksapa@gmail.com>
2024-12-17 16:34:05 +0100

    'add generic views and ulrs'

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
+from .forms import TaskForm
+from .models import Task
 # Create your views here.
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'task_list.html'
+    context_object_name = 'tasks'
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'task_detail.html'
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'task_form.html'
+    success_url = reverse_lazy('task_list')
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'task_form.html'
+    success_url = reverse_lazy('task_list')
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'task_confirm_delete.html'
+    success_url = reverse_lazy('task_list')
